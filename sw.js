7717a694dbed3a85320ac42ee1e3b1e50fc5891f
vistoriacautelar <soares.thamyres@gmail.com>
2026-07-28 17:14:13 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
 // no próximo carregamento.
 // ============================================================================
 
-const CACHE_VERSION = "v20260728-2";
+const CACHE_VERSION = "v20260728-3";
 const CACHE_NAME = "vistoria-cautelar-" + CACHE_VERSION;
 
 // Arquivos do app shell que serão cacheados na instalação.
